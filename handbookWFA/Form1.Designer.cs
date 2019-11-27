89bc2adae862adcaffe81aa5daf1df286b234c0d
bereianna <43843244+bereianna@users.noreply.github.com>
2019-11-27 07:18:13 +0200

    Add files via upload

---
+++ handbookWFA/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace handbookWFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.r1 = new System.Windows.Forms.RadioButton();
+            this.r2 = new System.Windows.Forms.RadioButton();
+            this.M = new System.Windows.Forms.GroupBox();
+            this.r3 = new System.Windows.Forms.RadioButton();
+            this.r4 = new System.Windows.Forms.RadioButton();
+            this.r5 = new System.Windows.Forms.RadioButton();
+            this.P = new System.Windows.Forms.GroupBox();
+            this.r10 = new System.Windows.Forms.RadioButton();
+            this.r6 = new System.Windows.Forms.RadioButton();
+            this.r9 = new System.Windows.Forms.RadioButton();
+            this.r7 = new System.Windows.Forms.RadioButton();
+            this.r8 = new System.Windows.Forms.RadioButton();
+            this.M.SuspendLayout();
+            this.P.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(86, 220);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(369, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 38);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(286, 53);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Text clar:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(267, 133);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Text criptat:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(249, 220);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Text decriptat:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(369, 133);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 38);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(369, 220);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(214, 38);
+            this.textBox3.TabIndex = 11;
+            // 
+            // r1
+            // 
+            this.r1.AutoSize = true;
+            this.r1.Location = new System.Drawing.Point(9, 19);
+            this.r1.Name = "r1";
+            this.r1.Size = new System.Drawing.Size(47, 17);
+            this.r1.TabIndex = 12;
+            this.r1.TabStop = true;
+            this.r1.Text = "CBD";
+            this.r1.UseVisualStyleBackColor = true;
+            // 
+            // r2
+            // 
+            this.r2.AutoSize = true;
+            this.r2.Location = new System.Drawing.Point(11, 42);
+            this.r2.Name = "r2";
+            this.r2.Size = new System.Drawing.Size(45, 17);
+            this.r2.TabIndex = 13;
+            this.r2.TabStop = true;
+            this.r2.Text = "CFB";
+            this.r2.UseVisualStyleBackColor = true;
+            // 
+            // M
+            // 
+            this.M.Controls.Add(this.r5);
+            this.M.Controls.Add(this.r1);
+            this.M.Controls.Add(this.r4);
+            this.M.Controls.Add(this.r2);
+            this.M.Controls.Add(this.r3);
+            this.M.Location = new System.Drawing.Point(12, 19);
+            this.M.Name = "M";
+            this.M.Size = new System.Drawing.Size(78, 142);
+            this.M.TabIndex = 14;
+            this.M.TabStop = false;
+            this.M.Text = "Mode";
+            this.M.PaddingChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // r3
+            // 
+            this.r3.AutoSize = true;
+            this.r3.Location = new System.Drawing.Point(11, 66);
+            this.r3.Name = "r3";
+            this.r3.Size = new System.Drawing.Size(46, 17);
+            this.r3.TabIndex = 15;
+            this.r3.TabStop = true;
+            this.r3.Text = "CTS";
+            this.r3.UseVisualStyleBackColor = true;
+            // 
+            // r4
+            // 
+            this.r4.AutoSize = true;
+            this.r4.Location = new System.Drawing.Point(11, 90);
+            this.r4.Name = "r4";
+            this.r4.Size = new System.Drawing.Size(46, 17);
+            this.r4.TabIndex = 16;
+            this.r4.TabStop = true;
+            this.r4.Text = "ECB";
+            this.r4.UseVisualStyleBackColor = true;
+            // 
+            // r5
+            // 
+            this.r5.AutoSize = true;
+            this.r5.Location = new System.Drawing.Point(11, 114);
+            this.r5.Name = "r5";
+            this.r5.Size = new System.Drawing.Size(46, 17);
+            this.r5.TabIndex = 17;
+            this.r5.TabStop = true;
+            this.r5.Text = "OFB";
+            this.r5.UseVisualStyleBackColor = true;
+            // 
+            // P
+            // 
+            this.P.Controls.Add(this.r10);
+            this.P.Controls.Add(this.r6);
+            this.P.Controls.Add(this.r9);
+            this.P.Controls.Add(this.r7);
+            this.P.Controls.Add(this.r8);
+            this.P.Location = new System.Drawing.Point(134, 19);
+            this.P.Name = "P";
+            this.P.Size = new System.Drawing.Size(105, 142);
+            this.P.TabIndex = 18;
+            this.P.TabStop = false;
+            this.P.Text = "Padding";
+            // 
+            // r10
+            // 
+            this.r10.AutoSize = true;
+            this.r10.Location = new System.Drawing.Point(11, 114);
+            this.r10.Name = "r10";
+            this.r10.Size = new System.Drawing.Size(52, 17);
+            this.r10.TabIndex = 17;
+            this.r10.TabStop = true;
+            this.r10.Text = "Zeros";
+            this.r10.UseVisualStyleBackColor = true;
+            // 
+            // r6
+            // 
+            this.r6.AutoSize = true;
+            this.r6.Location = new System.Drawing.Point(9, 19);
+            this.r6.Name = "r6";
+            this.r6.Size = new System.Drawing.Size(75, 17);
+            this.r6.TabIndex = 12;
+            this.r6.TabStop = true;
+            this.r6.Text = "ANSIX923";
+            this.r6.UseVisualStyleBackColor = true;
+            // 
+            // r9
+            // 
+            this.r9.AutoSize = true;
+            this.r9.Location = new System.Drawing.Point(11, 90);
+            this.r9.Name = "r9";
+            this.r9.Size = new System.Drawing.Size(59, 17);
+            this.r9.TabIndex = 16;
+            this.r9.TabStop = true;
+            this.r9.Text = "PKCS7";
+            this.r9.UseVisualStyleBackColor = true;
+            // 
+            // r7
+            // 
+            this.r7.AutoSize = true;
+            this.r7.Location = new System.Drawing.Point(11, 42);
+            this.r7.Name = "r7";
+            this.r7.Size = new System.Drawing.Size(73, 17);
+            this.r7.TabIndex = 13;
+            this.r7.TabStop = true;
+            this.r7.Text = "ISO10126";
+            this.r7.UseVisualStyleBackColor = true;
+            // 
+            // r8
+            // 
+            this.r8.AutoSize = true;
+            this.r8.Location = new System.Drawing.Point(11, 66);
+            this.r8.Name = "r8";
+            this.r8.Size = new System.Drawing.Size(51, 17);
+            this.r8.TabIndex = 15;
+            this.r8.TabStop = true;
+            this.r8.Text = "None";
+            this.r8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 436);
+            this.Controls.Add(this.P);
+            this.Controls.Add(this.M);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.M.ResumeLayout(false);
+            this.M.PerformLayout();
+            this.P.ResumeLayout(false);
+            this.P.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton r1;
+        private System.Windows.Forms.RadioButton r2;
+        private System.Windows.Forms.GroupBox M;
+        private System.Windows.Forms.RadioButton r5;
+        private System.Windows.Forms.RadioButton r4;
+        private System.Windows.Forms.RadioButton r3;
+        private System.Windows.Forms.GroupBox P;
+        private System.Windows.Forms.RadioButton r10;
+        private System.Windows.Forms.RadioButton r6;
+        private System.Windows.Forms.RadioButton r9;
+        private System.Windows.Forms.RadioButton r7;
+        private System.Windows.Forms.RadioButton r8;
+    }
+}
+
---
+++ handbookWFA/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace handbookWFA
+{
+    
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+        private static string Readfile()
+        {
+            TextReader s = new StreamReader("textCLAR.txt");
+            string b = s.ReadToEnd();
+            return b;
+        }
+        public void EncryptAesManaged(string raw)
+        {
+            try
+            {   
+                using (AesManaged aes = new AesManaged())
+                {
+                    if (r1.Checked == true)
+                        aes.Mode = CipherMode.CBC;
+                    if (r2.Checked == true)
+                        aes.Mode = CipherMode.CBC;
+                    if (r3.Checked == true)
+                        aes.Mode = CipherMode.CBC;
+                    if (r4.Checked == true)
+                        aes.Mode = CipherMode.CBC;
+                    if (r5.Checked == true)
+                        aes.Mode = CipherMode.CBC;
+
+                    if (r6.Checked == true)
+                        aes.Padding= PaddingMode.ANSIX923;
+                    if (r7.Checked == true)
+                        aes.Padding = PaddingMode.ISO10126;
+                    if (r8.Checked == true)
+                        aes.Padding = PaddingMode.None;
+                    if (r9.Checked == true)
+                        aes.Padding = PaddingMode.PKCS7;
+                    if (r10.Checked == true)
+                        aes.Padding = PaddingMode.Zeros;
+
+                    textBox1.Text = raw;
+                    byte[] encrypted = Encrypt(raw, aes.Key, aes.IV);
+                    textBox2.Text=System.Text.Encoding.UTF8.GetString(encrypted);
+                    string decrypted = Decrypt(encrypted, aes.Key, aes.IV);
+                    textBox3.Text = decrypted;
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+        }
+
+        static byte[] Encrypt(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;  
+            using (AesManaged aes = new AesManaged())
+            {
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+                using (MemoryStream ms = new MemoryStream())
+                {  
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {  
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(plainText);
+                        encrypted = ms.ToArray();
+                    }
+                }
+            } 
+            return encrypted;
+        }
+        static string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            using (AesManaged aes = new AesManaged())
+            {  
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {  using (StreamReader reader = new StreamReader(cs))
+                            plaintext = reader.ReadToEnd();
+                    }
+                }
+            }
+            return plaintext;
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string c = Readfile();
+            EncryptAesManaged(c);
+        }
+    }
+}
